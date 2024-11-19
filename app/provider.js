9b21a7982ad6b7f85429643b55026e4107d476f0
Lakshitha Vimuth <lvimuthfb@gmail.com>
2024-11-19 15:59:58 +0400

    Inngest

--- app/provider.js
+++ app/provider.js
@@ -3,6 +3,7 @@
 import { db } from "@/configs/db";
 import { USER_TABLE } from "@/configs/schema";
 import { useUser } from "@clerk/nextjs";
+import axios from "axios";
 import { eq } from "drizzle-orm";
 import React, { useEffect } from "react";
 
@@ -15,21 +16,24 @@ function Provider({ children }) {
 
   const CheckIsNewUser = async () => {
     // Check if the user already exists
-    const result = await db
-      .select()
-      .from(USER_TABLE)
-      .where(eq(USER_TABLE.email, user?.primaryEmailAddress?.emailAddress));
-    // If not then create a new
-    if (result.length == 0) {
-      const userResp = await db
-        .insert(USER_TABLE)
-        .values({
-          name: user?.fullName,
-          email: user?.primaryEmailAddress?.emailAddress,
-        })
-        .returning({ id: USER_TABLE.id });
-      console.log("New user created with ID:", userResp);
-    }
+    // const result = await db
+    //   .select()
+    //   .from(USER_TABLE)
+    //   .where(eq(USER_TABLE.email, user?.primaryEmailAddress?.emailAddress));
+    // // If not then create a new
+    // if (result.length == 0) {
+    //   const userResp = await db
+    //     .insert(USER_TABLE)
+    //     .values({
+    //       name: user?.fullName,
+    //       email: user?.primaryEmailAddress?.emailAddress,
+    //     })
+    //     .returning({ id: USER_TABLE.id });
+    //   console.log("New user created with ID:", userResp);
+    // }
+
+    const resp = await axios.post("/api/create-user", { user: user });
+    console.log(resp.data);
   };
   return <div>{children}</div>;
 }
---
+++ inngest/functions.js
@@ -0,0 +1,47 @@
+import { inngest } from "./client";
+import { db } from "@/configs/db";
+import { USER_TABLE } from "@/configs/schema";
+import { eq } from "drizzle-orm";
+
+export const helloWorld = inngest.createFunction(
+  { id: "hello-world" },
+  { event: "test/hello.world" },
+  async ({ event, step }) => {
+    await step.sleep("wait-a-moment", "1s");
+    return { event, body: "hello world" };
+  }
+);
+
+export const CreateNewUser = inngest.createFunction(
+  { id: "create-user" },
+  { event: "user.create" },
+  async ({ event, step }) => {
+    const user = event.data;
+    // Get event data
+    const result = await step.run(
+      "Check user and create new if not in DB",
+      async () => {
+        const result = await db
+          .select()
+          .from(USER_TABLE)
+          .where(eq(USER_TABLE.email, user?.primaryEmailAddress?.emailAddress));
+        // If not then create a new
+        if (result.length == 0) {
+          const userResp = await db
+            .insert(USER_TABLE)
+            .values({
+              name: user?.fullName,
+              email: user?.primaryEmailAddress?.emailAddress,
+            })
+            .returning({ id: USER_TABLE.id });
+          return userResp;
+        }
+        return result;
+      }
+    );
+    return "Success";
+  }
+  // Step is to send welcome email
+
+  //
+);
