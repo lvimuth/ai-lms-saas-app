533f3ba35c01f141af0a1373be98161d3c5d7c8c
Lakshitha Vimuth <lvimuthfb@gmail.com>
2024-11-20 00:03:20 +0400

    image added

---
+++ app/create/page.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Create() {
+  return (
+    <div className="flex flex-col items-center p-5 md:px-24 lg:px-36 mt-20">
+      <h2 className="font-bold text-3xl text-primary">
+        Start Building Your Personal Study Material
+      </h2>
+      <p className="text-gray-500">
+        Fill the details in order to generate study material for you
+      </p>
+    </div>
+  );
+}
+
+export default Create;
