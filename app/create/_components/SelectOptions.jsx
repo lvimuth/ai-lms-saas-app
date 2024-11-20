4036947c46818d68b58aa80d6ec38bc109cd1cc4
Lakshitha Vimuth <lvimuthfb@gmail.com>
2024-11-20 14:22:03 +0400

    image added

---
+++ app/create/_components/SelectOptions.jsx
@@ -0,0 +1,51 @@
+import Image from "next/image";
+import React, { useState } from "react";
+
+function SelectOptions() {
+  const [selectedOption, setSelectedOption] = useState();
+  const Options = [
+    {
+      name: "Exam",
+      icon: "/exam-results.png",
+    },
+    {
+      name: "Job Interview",
+      icon: "/interview.png",
+    },
+    {
+      name: "Practice",
+      icon: "/tennis.png",
+    },
+    {
+      name: "Coding Prep",
+      icon: "/coding.png",
+    },
+    {
+      name: "Other",
+      icon: "/brain.png",
+    },
+  ];
+  return (
+    <div className="">
+      <h2 className="text-center mb-2 text-lg">
+        For Which you want to create your personal study material?
+      </h2>
+      <div className="grid grid-cols-2 mt-2 md:grid-cols-3 lg:grid-cols-5 gap-2">
+        {Options.map((option, index) => (
+          <div
+            key={index}
+            className={`p-4 flex flex-col items-center justify-center border rounded-xl hover:border-primary cursor-pointer ${
+              option?.name == selectedOption && "border-primary bg-blue-50"
+            }`}
+            onClick={() => setSelectedOption(option.name)}
+          >
+            <Image src={option.icon} alt={option.name} width={50} height={50} />
+            <h2 className="text-sm mt-2">{option.name}</h2>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default SelectOptions;
--- app/create/page.jsx
+++ app/create/page.jsx
@@ -1,14 +1,19 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
+import SelectOptions from "./_components/SelectOptions";
 
 function Create() {
+  const [step, setStep] = useState(0);
   return (
     <div className="flex flex-col items-center p-5 md:px-24 lg:px-36 mt-20">
-      <h2 className="font-bold text-3xl text-primary">
+      <h2 className="font-bold text-4xl text-primary">
         Start Building Your Personal Study Material
       </h2>
-      <p className="text-gray-500">
+      <p className="text-gray-500 text-lg">
         Fill the details in order to generate study material for you
       </p>
+
+      <div className="mt-10">{step == 0 ? <SelectOptions /> : null}</div>
     </div>
   );
 }
