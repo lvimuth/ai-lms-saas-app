bde1309045ed9a6765ad76f66e5e1e219cf61a22
Lakshitha Vimuth <lvimuthfb@gmail.com>
2024-11-20 20:20:59 +0400

    table created

--- configs/schema.js
+++ configs/schema.js
@@ -1,8 +1,19 @@
-import { varchar, boolean, pgTable, serial } from "drizzle-orm/pg-core";
+import { varchar, boolean, pgTable, serial, json } from "drizzle-orm/pg-core";
 
 export const USER_TABLE = pgTable("users", {
   id: serial().primaryKey(),
   name: varchar().notNull(),
   email: varchar().notNull(),
   isMember: boolean().default(false),
+});
+
+export const STUDY_MATERIAL = pgTable("studyMaterial", {
+  id: serial().primaryKey(),
+  courseId: varchar().notNull(),
+  courseType: varchar().notNull(),
+  topic: varchar().notNull(),
+  duffecultyLevel: varchar().default("Easy"),
+  courseLayout: json(),
+  createdBy: varchar().notNull(),
+  status:varchar().default('generating'),
 });
